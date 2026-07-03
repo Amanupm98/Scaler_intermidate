1ddc53194f73c3060eb66a774cde46619281f637
Aman <aman.upm15@gmail.com>
2026-07-04 00:52:13 +0530

    Intermediate-1 class problems for Carry Forward

--- src/arrayscarryforward/sessionproblems/LengthofSmallestSubArrayOptimized.java
+++ src/arrayscarryforward/sessionproblems/LengthofSmallestSubArrayOptimized.java
@@ -2,37 +2,39 @@ package arrayscarryforward.sessionproblems;
 
 //Return length of smallest subarray which contain both max and min of array.
 // arr={2,2,6,4,5,1,5,2,6,4,1}
-//Bruite Force Approach
+// Need to do it again this code is incorrect
+public class LengthofSmallestSubArrayOptimized {
 
-
-public class LengthofSmallestSubArray {
     public static void main(String[] args) {
-        int[] arr = {2,2,6,4,5,1,5,2,6,4,1};
+        int[] arr = {6,3,6,4,1,6,5,2,1,4,1};
         int max = max(arr);
         int min = min(arr);
+        int latestMax = Integer.MAX_VALUE;
+        int latestMin = Integer.MAX_VALUE;
         int ans = Integer.MAX_VALUE;
-        for(int i=0;i<arr.length;i++){
+        for(int i=0; i<arr.length; i++){
             if(arr[i]==max){
-                for(int j=i+1; j<arr.length; j++){
-                    if(arr[j]==min){
-                        ans=min(ans,j-i+1);
-                        break;
-                    }
-                }
-            } else if(arr[i]==min) {
-                for(int j = i+1;j<arr.length; j++){
-                    if(arr[j]==max){
-                        ans=min(ans,j-i+1);
-                        break;
-                    }
-                }
+                latestMax=i;
+                ans=min(ans, max(latestMax,latestMin) - min(latestMax,latestMin) + 1);
+            }else if(arr[i]==min){
+                latestMin=i;
+                ans=min(ans, max(latestMax,latestMin) - min(latestMax,latestMin) + 1);
             }
+
+
         }
         System.out.println(ans);
+
+
+
     }
 
-    private static int min(int ans, int i) {
-        return Math.min(ans, i);
+    private static int min(int latestMax, int latestMin) {
+       return Math.min(latestMin,latestMax);
+    }
+
+    private static int max(int latestMax, int latestMin) {
+        return Math.max(latestMax,latestMin);
     }
 
     private static int max(int[] arr) {
@@ -54,6 +56,4 @@ public class LengthofSmallestSubArray {
         }
         return min;
     }
-
-
 }
